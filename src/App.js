3dfdf43830d479068b20a7d63a9808185441b5f1
Taizul Islam <57869624+Mohammad-Taizul-Islam@users.noreply.github.com>
2025-11-17 08:05:37 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
+
+export default function List() {
+  const chemists = people.filter((person) => person.profession === "chemist");
+
+  const newOtherItems = people.filter(
+    (person) => person.profession !== "Chemist"
+  );
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <h2>Chemist</h2>
+      <ul>
+        {chemists.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+      <h2>Everyone Else</h2>
+      <ul>
+        {newOtherItems.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl(person)} alt={person.name} />
+            <p>
+              <b>{person.name}:</b>
+              {" " + person.profession + " "}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </article>
+  );
+}
